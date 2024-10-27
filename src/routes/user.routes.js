72e4a766b161135016c9f87b97fa95fbf4a02df3
sai vinay <saiv71155@gmail.com>
2024-10-27 12:48:11 +0530

    add refreshtoken endpoint

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,7 @@
 import {Router} from "express";
-import {registerUser} from "../controllers/user.controller.js"
+import {registerUser,loginUser,logoutuser,refreshaccesstoken} from "../controllers/user.controller.js"
 import {upload} from "../middlewares/multer.middleware.js";
-
+import { verifyjwt } from "../middlewares/auth.middleware.js";
 const router=Router()
 router.route("/register").post(
     upload.fields([
@@ -17,4 +17,8 @@ router.route("/register").post(
     registerUser
     )
 
+router.route("/login").post(loginUser)
+
+router.route("/logout").post(verifyjwt,logoutuser)
+router.route.("/refreshtoken").post(refreshaccesstoken)
 export default router
